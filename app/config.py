a9c4f01a50be8be4c34236a3872bbc001f818a9d
Cozyasia <cozy.asias@gmail.com>
2025-10-08 10:17:09 +0300

    Delete app/config.py

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-PUBLIC_URL = os.environ["PUBLIC_URL"].rstrip("/")  # напр. https://gpt5pro-bot.onrender.com
-WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET", "hook-" + os.urandom(8).hex())
-PORT = int(os.environ.get("PORT", "10000"))  # Render прокидывает PORT сам
