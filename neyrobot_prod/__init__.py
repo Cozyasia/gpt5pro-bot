2c1422628a67aa4dd91162ae7c8c139a4232fd79
Cozyasia <cozy.asias@gmail.com>
2026-07-25 18:04:55 +0300

    Publish V206 selfie command routing release

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production hardening package."""
 
-VERSION = "v205-selfie-persistent-catalog-2026-07-25"
+VERSION = "v206-selfie-command-routing-2026-07-25"
 
 __all__ = ["VERSION"]
