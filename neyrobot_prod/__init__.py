ab8fc73731c44ddc7c791f450265a260ca215a75
Cozyasia <cozy.asias@gmail.com>
2026-07-25 14:00:31 +0300

    Publish v202 selfie admin access fix

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production hardening package."""
 
-VERSION = "v201-credit-selfie-baseline-2026-07-25"
+VERSION = "v202-selfie-admin-access-2026-07-25"
 
 __all__ = ["VERSION"]
