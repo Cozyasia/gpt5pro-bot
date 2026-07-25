247fe882a7f023ebfb951eff20cc7a303579f1f2
Cozyasia <cozy.asias@gmail.com>
2026-07-25 16:36:45 +0300

    Publish V204 Comet multi-reference selfie release

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production hardening package."""
 
-VERSION = "v203-selfie-four-reference-gemini-2026-07-25"
+VERSION = "v204-selfie-comet-multireference-2026-07-25"
 
 __all__ = ["VERSION"]
