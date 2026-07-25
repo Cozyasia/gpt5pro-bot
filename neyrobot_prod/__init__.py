886d732e992038321b7bd79c61dbc3644aa9519c
Cozyasia <cozy.asias@gmail.com>
2026-07-25 03:55:07 +0300

    Publish v201 credit and selfie baseline version

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production hardening package."""
 
-VERSION = "v119-production-hardening-2026-07-18"
+VERSION = "v201-credit-selfie-baseline-2026-07-25"
 
 __all__ = ["VERSION"]
