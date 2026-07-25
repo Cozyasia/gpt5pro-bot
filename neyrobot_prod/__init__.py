1b5a6d28495d2995a9843949fc7be30bcdb37a08
Cozyasia <cozy.asias@gmail.com>
2026-07-25 17:36:55 +0300

    Publish V205 persistent selfie catalog release

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production hardening package."""
 
-VERSION = "v204-selfie-comet-multireference-2026-07-25"
+VERSION = "v205-selfie-persistent-catalog-2026-07-25"
 
 __all__ = ["VERSION"]
