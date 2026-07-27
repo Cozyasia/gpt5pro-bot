be278dda92a055833b131b05cc04cdbaf28c7199
Cozyasia <cozy.asias@gmail.com>
2026-07-27 19:00:17 +0300

    Release V224 user face-anchor selfie pipeline

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v223-selfie-deterministic-scene-owner-2026-07-27"
+VERSION = "v224-selfie-user-face-anchor-scene-lock-2026-07-27"
 
 __all__ = ["VERSION"]
