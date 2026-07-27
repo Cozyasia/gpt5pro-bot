6edc001664c72d9742635a978151fa85c16346f4
Cozyasia <cozy.asias@gmail.com>
2026-07-28 02:17:53 +0300

    Release fixed V226 direct Google route

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v226-selfie-direct-google-key-proof-2026-07-28"
+VERSION = "v226-selfie-direct-google-key-proof-fixed-2026-07-28"
 
 __all__ = ["VERSION"]
