451a49cdef4ad0fac67873dd4497c9a2d8169110
Cozyasia <cozy.asias@gmail.com>
2026-07-27 18:38:45 +0300

    Promote strict triref scene-base selfie runtime to V222

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v221-selfie-identity-scene-lock-2026-07-27"
+VERSION = "v222-selfie-exact-triref-scene-base-2026-07-27"
 
 __all__ = ["VERSION"]
