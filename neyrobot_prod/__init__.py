aea4dd2a0c0d1741c4773f952be7d5a6d8f12b94
Cozyasia <cozy.asias@gmail.com>
2026-07-28 01:58:59 +0300

    Set package version to V226 key proof

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v225-selfie-direct-gemini-pro-2026-07-27"
+VERSION = "v226-selfie-direct-google-key-proof-2026-07-28"
 
 __all__ = ["VERSION"]
