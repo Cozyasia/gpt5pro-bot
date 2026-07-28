198969f3809bd5d06bee6f9657e1a95e61f24690
Cozyasia <cozy.asias@gmail.com>
2026-07-29 02:04:16 +0300

    Release V233 body-first face-transplant pipeline

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v232-selfie-hard-source-google-two-stage-2026-07-29"
+VERSION = "v233-selfie-body-face-transplant-google-2026-07-29"
 
 __all__ = ["VERSION"]
