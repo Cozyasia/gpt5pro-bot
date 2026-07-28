146630e88f853cf7aac6f396e6ef386873801ae1
Cozyasia <cozy.asias@gmail.com>
2026-07-28 19:48:07 +0300

    Release V228 forced direct Google selfie owner

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v227-selfie-hard-direct-google-handler-2026-07-28"
+VERSION = "v228-selfie-force-direct-google-owner-2026-07-28"
 
 __all__ = ["VERSION"]
