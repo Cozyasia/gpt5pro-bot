d3eb03b1b71628d8b2b2cfc69460c34abe9da91c
Cozyasia <cozy.asias@gmail.com>
2026-07-28 22:16:50 +0300

    Release V230 single-owner two-stage Google selfie route

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v229-selfie-canonical-two-stage-google-2026-07-28"
+VERSION = "v230-selfie-single-owner-two-stage-google-2026-07-28"
 
 __all__ = ["VERSION"]
