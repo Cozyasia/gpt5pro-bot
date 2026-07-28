38c32f09899fa5c7517de22493bf81cc646f4afe
Cozyasia <cozy.asias@gmail.com>
2026-07-29 01:44:14 +0300

    Release V232 hard-source Google selfie owner

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v231-selfie-source-canonical-two-stage-google-2026-07-28"
+VERSION = "v232-selfie-hard-source-google-two-stage-2026-07-29"
 
 __all__ = ["VERSION"]
