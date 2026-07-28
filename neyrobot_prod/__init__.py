76c4b3eb006bd86669a3885ec5cb1b0d8964dcf1
Cozyasia <cozy.asias@gmail.com>
2026-07-28 21:28:12 +0300

    Release V229 canonical two-stage Google selfie pipeline

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v228-selfie-force-direct-google-owner-2026-07-28"
+VERSION = "v229-selfie-canonical-two-stage-google-2026-07-28"
 
 __all__ = ["VERSION"]
