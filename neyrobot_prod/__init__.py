6d136f01f8d529ce1acf268b94dc594bc186f1f4
Cozyasia <cozy.asias@gmail.com>
2026-07-29 13:14:55 +0300

    Publish V235 hard selfie owner version

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v234-selfie-hybrid-real-faceswap-2026-07-29"
+VERSION = "v235-selfie-hard-owner-fullbody-faceswap-2026-07-29"
 
 __all__ = ["VERSION"]
