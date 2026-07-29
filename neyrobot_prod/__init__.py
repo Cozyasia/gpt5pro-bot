59436de4da4f3ad8f8ff89d724a1e8334dcb1112
Cozyasia <cozy.asias@gmail.com>
2026-07-29 14:46:56 +0300

    Mark Celebrity Selfie mode as removed

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v236-selfie-clean-rewrite-fullbody-faceswap-2026-07-29"
+VERSION = "v237-celebrity-selfie-removed-2026-07-29"
 
 __all__ = ["VERSION"]
