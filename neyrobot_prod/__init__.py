0ce8414dbc4d9663fe5fe490a1d6a67de1695441
Cozyasia <cozy.asias@gmail.com>
2026-08-19 15:55:49 +0300

    Promote V242 source-expression lock release

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v241-authoritative-selfie-runtime-2026-08-19"
+VERSION = "v242-source-expression-lock-2026-08-19"
 
 __all__ = ["VERSION"]
