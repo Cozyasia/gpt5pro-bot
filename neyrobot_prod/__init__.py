295d3100ceed3c58f7f7c8a96126e0eb3224adea
Cozyasia <cozy.asias@gmail.com>
2026-08-19 11:09:25 +0300

    Promote V240 selfie quality resilience version

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v239-single-owner-front-camera-source-expression-2026-08-19"
+VERSION = "v240-pro-fast-fail-face-roi-2026-08-19"
 
 __all__ = ["VERSION"]
