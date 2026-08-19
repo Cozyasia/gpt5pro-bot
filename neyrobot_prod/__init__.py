1d4367382062c3fa5f78b6f17f5be8dbc87edee1
Cozyasia <cozy.asias@gmail.com>
2026-08-19 11:43:43 +0300

    Promote V241 authoritative selfie runtime version

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v240-pro-fast-fail-face-roi-2026-08-19"
+VERSION = "v241-authoritative-selfie-runtime-2026-08-19"
 
 __all__ = ["VERSION"]
