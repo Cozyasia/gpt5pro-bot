25385f5c4428c2a19e0e65b1ad49eba3e766b3a1
Cozyasia <cozy.asias@gmail.com>
2026-08-19 16:49:13 +0300

    Promote V244 final selfie runtime lock

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v243-source-guided-face-detail-2026-08-19"
+VERSION = "v244-final-runtime-lock-2026-08-19"
 
 __all__ = ["VERSION"]
