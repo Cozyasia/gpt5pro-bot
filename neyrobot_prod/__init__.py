07648723f1dc838b4995fac566452c973fe0e534
Cozyasia <cozy.asias@gmail.com>
2026-08-19 20:50:35 +0300

    Bump package version to V247 quality-only overlay

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v246-lossless-target-detail-ux-hardlock-2026-08-19"
+VERSION = "v247-provider-supersample-detail-lock-2026-08-19"
 
 __all__ = ["VERSION"]
