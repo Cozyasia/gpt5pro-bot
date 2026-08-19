59d4298e6985d6724140cb43f82e8cc9279be0bc
Cozyasia <cozy.asias@gmail.com>
2026-08-19 18:58:54 +0300

    Bump production package to V245 clean real FaceSwap release

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v244-final-runtime-lock-2026-08-19"
+VERSION = "v245-clean-real-faceswap-ux-lock-2026-08-19"
 
 __all__ = ["VERSION"]
