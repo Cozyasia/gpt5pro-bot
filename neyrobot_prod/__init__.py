9de830de232f4b7025e7fc4e6343c24bc9aacc58
Cozyasia <cozy.asias@gmail.com>
2026-08-22 01:31:52 +0300

    Bump production package to V255

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v254-landmark-fit-seamless-source-2026-08-22"
+VERSION = "v255-source-face-gate-lossless-2026-08-22"
 
 __all__ = ["VERSION"]
