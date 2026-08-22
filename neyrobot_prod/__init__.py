4b1349b015c6a8babd0643078b650e8c68c07c60
Cozyasia <cozy.asias@gmail.com>
2026-08-22 12:25:19 +0300

    Set package version to V256

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Neyro-Bot production package."""
 
-VERSION = "v255-source-face-gate-lossless-2026-08-22"
+VERSION = "v256-large-scale-source-pixels-2026-08-22"
 
 __all__ = ["VERSION"]
