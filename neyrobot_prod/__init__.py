f79d755cc606fda0df33cd8cbcf748cc9ec365f7
Cozyasia <cozy.asias@gmail.com>
2026-08-26 03:01:04 +0300

    Bump production package to V260

--- neyrobot_prod/__init__.py
+++ neyrobot_prod/__init__.py
@@ -8,6 +8,7 @@
 # v256-large-scale-source-pixels-2026-08-22
 # v257-native-sampling-guard-2026-08-22
 # v258-inner-face-integration-2026-08-24
-VERSION = "v259-eye-landmark-protection-2026-08-26"
+# v259-eye-landmark-protection-2026-08-26
+VERSION = "v260-eye-roi-memory-safe-2026-08-26"
 
 __all__ = ["VERSION"]
